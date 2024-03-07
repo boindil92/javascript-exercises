90cbf7b822d8aacc95e172668b82d94666e6ab59
Seba <talahammen@gmail.com>
2024-03-07 18:57:53 +0100

    mby im arriving somewhere... but it is time to go.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,9 @@
-const removeFromArray = function(array,ar) {
+const removeFromArray = function(array,...ar) {
 
     let i = 0;
     let newArray=[];
 
-    while (i < array.length) {
+    while (i < array.length) {                     //devi fare questo per ogni elemento dato di ...ar 
         if (array[i] != ar) {
             newArray.push(array[i]);
         }
