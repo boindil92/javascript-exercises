ea3aa35fed3201303e47e8366f93e81069b08aa0
Seba <talahammen@gmail.com>
2024-03-08 18:46:41 +0100

    Ex 5 working.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
 
+    let c=(Math.abs(a-b)+1);
+    let d=0;
+    
+
+    for (let i=0;i<c;i++){
+
+       d=(d+(a+i))
+
+    }
+
+    return d;
+
+
+
+
+    
 };
 
 // Do not edit below this line
