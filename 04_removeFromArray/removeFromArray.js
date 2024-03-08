50de2566386f723351a6d9f78d3b785c71f4aae9
Seba <talahammen@gmail.com>
2024-03-08 16:41:36 +0100

    explenation.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -4,13 +4,14 @@ const removeFromArray = function(array,...disc) {
     
 
     for (let i=0; i<array.length; i++) {
-        if (disc.indexOf(array[i])=== -1) {
+        if (disc.indexOf(array[i])=== -1) { 
             newArr.push(array[i]);
         }
     }
 
 
-    
+//disc.index cerca l' indice di posizione del valore da scartare, all'interno di array
+//alla posizione i, se il risultato è stretto uguale di -1 vuol dire che non è presente.  
 
     
     return newArr;
