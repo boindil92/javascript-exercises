9a59d6319ebf617987c0538436a5ea070a0d57bc
Seba <talahammen@gmail.com>
2024-03-08 10:24:29 +0100

    unexpected error occurred and fixed.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,15 @@
-const removeFromArray = function(array,...ar) {
+const removeFromArray = function(array,ar) {
 
-    let i = 0;
+    let i=0;
     let newArray=[];
 
-    while (i < array.length) {                     //devi fare questo per ogni elemento dato di ...ar 
-        if (array[i] != ar) {
-            newArray.push(array[i]);
+    while (i<array.length) {
+
+        if (array[i] != ar){
+        newArray.push(array[i]);
         }
-  
-    i++;
+        
+        i++;
     }
 
     return newArray;
