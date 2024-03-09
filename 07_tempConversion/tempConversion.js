a9ae3771f62321896e915ec2780b2e219088c628
Seba <talahammen@gmail.com>
2024-03-09 11:25:00 +0100

    ex 7 done. easy ^^ .

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  
+  const k=1.8;
+  let c= (f-32)/k;
+  let cRound=Math.round(c*10)/10;
+
+  return cRound;
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(a) {
+
+  b=a*1.8+32;
+  bOneDec= Math.round(b*10)/10;
+
+  return bOneDec;
+
 };
 
 // Do not edit below this line
